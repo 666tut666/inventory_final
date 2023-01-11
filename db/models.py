c716e1a68cf4e659cc50209c66fb43567970a575
666tut666 <666tut666@users.noreply.github.com>
2023-01-11 14:39:41 +0545

    first try: db for pending related db

--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 from db.database import Base
 from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, Date, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy_utils.types import ChoiceType
 
 
 class User(Base):
@@ -99,3 +100,37 @@ class Pending(Base):
     email = Column(String, unique=True, nullable=False, index=True)
     user_id = Column(Integer, ForeignKey("user.id"))
     admin_id = Column(Integer, ForeignKey("admin.id"))
+
+
+
+'''
+## Trying new method for pending, allowed, taken and returned
+    
+    class Order(Base):
+    
+        ORDER_STATUSES=(
+            ('PENDING','pending'),
+            ('IN-TRANSIT','in-transit'),
+            ('DELIVERED','delivered')
+    
+        )
+    
+        PIZZA_SIZES=(
+            ('SMALL','small'),
+            ('MEDIUM','medium'),
+            ('LARGE','large'),
+            ('EXTRA-LARGE','extra-large')
+        )
+    
+    
+        __tablename__='orders'
+        id=Column(Integer,primary_key=True)
+        quantity=Column(Integer,nullable=False)
+        order_status=Column(ChoiceType(choices=ORDER_STATUSES),default="PENDING")
+        pizza_size=Column(ChoiceType(choices=PIZZA_SIZES),default="SMALL")
+        user_id=Column(Integer,ForeignKey('user.id'))
+        user=relationship('User',back_populates='orders')
+    
+        def __repr__(self):
+            return f"<Order {self.id}>"
+'''
