505bf09f88b73b3b32b10037d4bc80fb7edd48b4
666tut666 <666tut666@users.noreply.github.com>
2023-01-11 14:51:14 +0545

    quoted new code

--- db/models.py
+++ db/models.py
@@ -101,6 +101,38 @@ class Pending(Base):
     user_id = Column(Integer, ForeignKey("user.id"))
     admin_id = Column(Integer, ForeignKey("admin.id"))
 
+'''
+class Order(Base):
+    ORDER_STATUSES = (
+        ('PENDING', 'pending'),
+        ('IN-TRANSIT', 'in-transit'),
+        ('DELIVERED', 'delivered')
+
+    )
+
+    PIZZA_SIZES = (
+        ('SMALL', 'small'),
+        ('MEDIUM', 'medium'),
+        ('LARGE', 'large'),
+        ('EXTRA-LARGE', 'extra-large')
+    )
+
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer, nullable=False)
+    order_status = Column(ChoiceType(choices=ORDER_STATUSES), default="PENDING")
+    pizza_size = Column(ChoiceType(choices=PIZZA_SIZES), default="SMALL")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='orders')
+
+    def __repr__(self):
+        return f"<Order {self.id}>"
+
+'''
+
+
+
+
 
 
 '''
@@ -134,3 +166,6 @@ class Pending(Base):
         def __repr__(self):
             return f"<Order {self.id}>"
 '''
+
+
+
--- db/schemas/schema.py
+++ db/schemas/schema.py
@@ -23,3 +23,36 @@ class User(BaseModel):
     user_id: int
     id: int
 
+
+
+'''
+## Trying new method for pending, allowed, taken and returned
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "quantity":2,
+                "pizza_size":"LARGE"
+            }
+        }
+
+
+class OrderStatusModel(BaseModel):
+    order_status:Optional[str]="PENDING"
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "order_status":"PENDING"
+            }
+        }
+'''
