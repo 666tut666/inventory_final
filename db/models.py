4f6515729b5bab3ef2390b2c9dbe948d214c9bcb
666tut666 <666tut666@users.noreply.github.com>
2023-01-12 07:55:45 +0545

    all working properly new code to be added

--- db/models.py
+++ db/models.py
@@ -112,23 +112,15 @@ class Pending(Base):
 class Order(Base):
     ORDER_STATUSES = (
         ('PENDING', 'pending'),
-        ('IN-TRANSIT', 'in-transit'),
-        ('DELIVERED', 'delivered')
-
-    )
-
-    PIZZA_SIZES = (
-        ('SMALL', 'small'),
-        ('MEDIUM', 'medium'),
-        ('LARGE', 'large'),
-        ('EXTRA-LARGE', 'extra-large')
+        ('ACKNOWLEDGE', 'acknowledge'),
+        ('TAKEN', 'taken'),
+        ('RETURNED','returned')
     )
 
     __tablename__ = 'orders'
     id = Column(Integer, primary_key=True)
     quantity = Column(Integer, nullable=False)
     order_status = Column(ChoiceType(choices=ORDER_STATUSES), default="PENDING")
-    pizza_size = Column(ChoiceType(choices=PIZZA_SIZES), default="SMALL")
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship('User', back_populates='orders')
 
--- db/schemas/schema.py
+++ db/schemas/schema.py
@@ -33,32 +33,12 @@ class OrderModel(BaseModel):
     id:Optional[int]
     quantity:int
     order_status:Optional[str]="PENDING"
-    pizza_size:Optional[str]="SMALL"
     user_id:Optional[int]
 
-
     class Config:
-        orm_mode=True
-        schema_extra={
-            "example":{
-                "quantity":2,
-                "pizza_size":"LARGE"
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "order_status": "PENDING"
             }
         }
-
-'''
-## Trying new method for pending, allowed, taken and returned
-
-
-
-class OrderStatusModel(BaseModel):
-    order_status:Optional[str]="PENDING"
-
-    class Config:
-        orm_mode=True
-        schema_extra={
-            "example":{
-                "order_status":"PENDING"
-            }
-        }
-'''
